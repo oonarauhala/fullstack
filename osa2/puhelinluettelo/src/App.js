e0b4c3ce4751f1e02e3031e2df5630bce51070d2
Oona Rauhala <oona.e.rauhala@gmail.com>
2022-01-27 15:02:15 +0200

    Add 2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const person = {
+      name: newName
+    }
+    setPersons(persons.concat(person))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Persons persons={persons} />
+    </div>
+  )
+}
+
+const Persons = ({ persons }) => {
+  const result = persons.map(person =>
+    <div key={person.name}>
+      <p>{person.name}</p>
+    </div>
+  )
+  return (
+    <>
+      {result}
+    </>
+  )
+}
+
+export default App
