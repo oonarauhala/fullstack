f00a3858a3991575a359bdb95952b9879f0468d1
Oona Rauhala <oona.e.rauhala@gmail.com>
2022-01-27 18:15:11 +0200

    Add 2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '12345'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [nameFilter, setNameFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -31,17 +32,36 @@ const App = () => {
     }
   }
 
+  const findPersons = () => {
+    const result = []
+    persons.forEach(person => {
+      if (person.name.toLowerCase().includes(nameFilter.toLowerCase())) {
+        result.push(person)
+      }
+    })
+    return result
+  }
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+    console.log(newName)
   }
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setNameFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>
+        filter shown with <input value={nameFilter} onChange={handleFilterChange} />
+      </p>
+      <h2>Add new entry</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -54,13 +74,14 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <Persons persons={persons} findPersons={findPersons} />
     </div>
   )
 }
 
-const Persons = ({ persons }) => {
-  const result = persons.map(person =>
+const Persons = ({ persons, findPersons }) => {
+  const filteredPersons = findPersons(persons)
+  const result = filteredPersons.map(person =>
     <div key={person.name}>
       <p>{person.name} {person.number}</p>
     </div>
