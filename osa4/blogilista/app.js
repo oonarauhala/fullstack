0e20c4731d59deb96d32b54e70513cb1b422d490
Oona Rauhala <oona.e.rauhala@gmail.com>
2022-02-08 17:18:34 +0200

    Add 4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+mongoose.connect(config.MONGODB_URL)
+    .then(() => {
+        logger.info("Connected to MongoDB")
+    })
+    .catch(error => {
+        logger.error('Error connecting to MongoDB', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+
+
+module.exports = app
---
+++ osa4/blogilista/utils/config.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+let PORT = process.env.PORT
+let MONGODB_URL = process.env.MONGODB_URL
+
+module.exports = {
+    MONGODB_URL, PORT
+}
