e42ddfb17d062081019fd393195aafcec8dcf30e
Oona Rauhala <oona.e.rauhala@gmail.com>
2022-02-10 15:40:51 +0200

    Add 4.10

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -3,33 +3,21 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
-const initialBlogs = [
-    {
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7
-    },
-    {
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5
-    }
-]
+const helper = require('./test_helper')
+
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
+    let blogObject = new Blog(helper.initialBlogs[0])
     await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
+    blogObject = new Blog(helper.initialBlogs[1])
     await blogObject.save()
 })
 
 describe('with blogs in a list', () => {
     test('the right number of blogs are returned', async () => {
         const response = await api.get('/api/blogs')
-        expect(response.body).toHaveLength(initialBlogs.length)
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
     })
     test('blogs are returned as json', async () => {
         await api
@@ -40,6 +28,28 @@ describe('with blogs in a list', () => {
     test('blogs are identified with a field `id`', async () => {
         const response = await api.get('/api/blogs')
         expect(response.body[0].id).toBeDefined()
+    })
+})
+
+describe('adding blog', () => {
+    test('increases the count of blogs by one', async () => {
+        await api
+            .post('/api/blogs')
+            .send(helper.oneBlog)
+            .expect(200)
+            .expect('Content-type', /application\/json/)
+        const notesAtEnd = await helper.blogsInDB()
+        expect(notesAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    })
+    test('added data is correct', async () => {
+        await api
+            .post('/api/blogs')
+            .send(helper.oneBlog)
+            .expect(200)
+            .expect('Content-type', /application\/json/)
+        const notesAtEnd = await helper.blogsInDB()
+        const titles = notesAtEnd.map(res => res.title)
+        expect(titles).toContain('Canonical string reduction')
     })
 })
 
