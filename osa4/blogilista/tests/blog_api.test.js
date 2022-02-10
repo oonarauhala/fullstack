1b829fad9b52ce89061e4167b203af6a890fbe4c
Oona Rauhala <oona.e.rauhala@gmail.com>
2022-02-10 14:35:56 +0200

    Add 4.8

---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const initialBlogs = [
+    {
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7
+    },
+    {
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
+
+describe('with blogs in a list', () => {
+    test('the right number of blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(initialBlogs.length)
+    })
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+})
+
+
+afterAll(() => {
+    mongoose.connection.close()
+})
--- osa4/blogilista/utils/config.js
+++ osa4/blogilista/utils/config.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 
 let PORT = process.env.PORT
-let MONGODB_URL = process.env.MONGODB_URL
+//let MONGODB_URL = process.env.MONGODB_URL
+let MONGODB_URL = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URL
+    : process.env.MONGODB_URL
 
 module.exports = {
     MONGODB_URL, PORT
