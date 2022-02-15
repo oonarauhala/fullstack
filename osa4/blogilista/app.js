5672c580d581e4b8f1c5ca310d83731f24464fa9
Oona Rauhala <oona.e.rauhala@gmail.com>
2022-02-15 16:15:06 +0200

    Add 4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,6 +1,7 @@
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -21,6 +22,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 
 
