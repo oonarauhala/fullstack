800ca7ea07f7a8742c47fa7e3bdaade5364c9c48
Oona Rauhala <oona.e.rauhala@gmail.com>
2022-02-16 12:55:41 +0200

    Add 4.20

--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -12,7 +12,18 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        request.token = authorization.substring(7)
+    } else {
+        request.token = null
+    }
+    next()
+}
+
 module.exports = {
     requestLogger,
-    unknownEndpoint
+    unknownEndpoint,
+    tokenExtractor
 }
