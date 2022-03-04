9594b80707ac69806956be9c1669e94b438ec4d2
oonarauhala <oona.e.rauhala@gmail.com>
2022-03-04 13:55:54 +0200

    Add 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,52 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  if (visible) {
+    return (
+      <div>
+        <div style={blogStyle}>
+          <div>
+            <p>{blog.title} {blog.author}</p>
+            <p>{blog.url}</p>
+            <div>
+              {blog.likes}
+              <button>like</button>
+            </div>
+            <p>{blog.user.username}</p>
+          </div>
+          <button onClick={toggleVisibility}>hide</button>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author}
+        </div>
+        <button onClick={toggleVisibility}>view</button>
+      </div> 
+    </div>
+  )
+}
 
 export default Blog
