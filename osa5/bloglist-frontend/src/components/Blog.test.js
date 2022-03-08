cb1172e75bace2d93f58d28c495a893a5d5334e9
oonarauhala <oona.e.rauhala@gmail.com>
2022-03-08 16:24:43 +0200

    Add 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-  test('renders blog in small', () => {
+  beforeEach(() => {
     const blog = {
       title: 'Testiblogi',
       author: 'Testaaja',
@@ -19,10 +20,24 @@ describe('<Blog />', () => {
     const mockRemoveBlog = jest.fn()
 
     render(<Blog blog={blog} handleLike={mockHandleLike} removeBlog={mockRemoveBlog}/>)
+  })
 
+  test('renders blog in small', () => {
     const title = screen.findByText('Testiblogi')
     const likeButton = screen.queryByText('like')
     expect(title).toBeDefined()
     expect(likeButton).toBeNull()
+  })
+
+  test('renders blog in big when opened', () => {
+    const button = screen.getByText('view')
+    userEvent.click(button)
+
+    const title = screen.findByText('Testiblogi')
+    const url = screen.findByText('testiblogi.com')
+    const likes = screen.findByText('20')
+    expect(title).toBeDefined()
+    expect(url).toBeDefined()
+    expect(likes).toBeDefined()
   })
 })
