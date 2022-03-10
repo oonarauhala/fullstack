b0f6f89050bb209be4c7350fa7df60a335e16add
oonarauhala <oona.e.rauhala@gmail.com>
2022-03-10 17:34:14 +0200

    Add 5.18

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,8 +1,15 @@
 // Backend to be run in dev mode (npm run dev)
+// npm run cypress:op
 
 describe('Blog app', function() {
     beforeEach(function() {
       cy.request('POST', 'http://localhost:3003/api/testing/reset')
+      const user  = {
+          username: 'Testi',
+          name: 'T. Testaaja',
+          password: 'asdf'
+      }
+      cy.request('POST', 'http://localhost:3003/api/users/', user)
       cy.visit('http://localhost:3000')
     })
   
@@ -12,5 +19,21 @@ describe('Blog app', function() {
       cy.contains('password')
       cy.contains('login')
     })
+
+    describe('Login',function() {
+        it('succeeds with correct credentials', function() {
+          cy.get('#username').type('Testi')
+          cy.get('#password').type('asdf')
+          cy.contains('login').click()
+          cy.contains('T. Testaaja logged in')
+        })
+    
+        it('fails with wrong credentials', function() {
+          cy.get('#username').type('Väärä')
+          cy.get('#password').type('salasana')
+          cy.contains('login').click()
+          cy.contains('Wrong username or password')
+        })
+      })
   })
   
