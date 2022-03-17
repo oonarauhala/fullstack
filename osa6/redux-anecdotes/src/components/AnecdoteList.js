ddf541413c5ea541aa36748b055f960a74192d72
oonarauhala <oona.e.rauhala@gmail.com>
2022-03-17 15:27:28 +0200

    Add 6.19

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,27 +1,17 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { likeAnecdote } from '../reducers/anecdoteReducer'
 import { notify } from '../reducers/notificationReducer'
 
 const AnecdoteList = props => {
-  const dispatch = useDispatch()
-
-  const anecdotes = useSelector(state => {
-    const query = state.filter
-    const filteredAnecdotes = state.anecdotes.filter(anecdote => 
-      anecdote.content.toLowerCase().indexOf(query.toLowerCase()) !== -1)
-    const sortedAnecdotes = filteredAnecdotes.sort((first, second) => 
-      (first.votes > second.votes) ? -1 : 1)
-    return sortedAnecdotes
-  })
 
   const vote = anecdote => {
-    dispatch(likeAnecdote(anecdote.id))
-    dispatch(notify(`You voted for '${anecdote.content}'`, 5000))
+    props.likeAnecdote(anecdote.id)
+    props.notify(`You voted for '${anecdote.content}'`, 5000)
   }
 
   return (
     <div>
-      {anecdotes.map(anecdote =>
+      {props.anecdotes.map(anecdote =>
       <div key={anecdote.id}>
         <div>
           {anecdote.content}
@@ -36,4 +26,23 @@ const AnecdoteList = props => {
   )
 }
 
-export default AnecdoteList
+const mapStateToProps = state => {    
+  const query = state.filter
+  const filteredAnecdotes = state.anecdotes.filter(anecdote => 
+    anecdote.content.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+  const sortedAnecdotes = filteredAnecdotes.sort((first, second) => 
+    (first.votes > second.votes) ? -1 : 1)
+  return {
+    anecdotes: sortedAnecdotes
+  }
+}
+
+const mapDispatchToProps = {
+  likeAnecdote, notify
+}
+
+const ConnectedAnecdoteList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+  )(AnecdoteList)
+export default ConnectedAnecdoteList
